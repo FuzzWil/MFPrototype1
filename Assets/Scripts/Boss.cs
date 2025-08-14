4025a5aafb3672beba7744642d57f1dc47647a7c
Florence Etienne <fuzzwil398@gmail.com>
2025-08-14 13:28:42 -0400

    needs to be reworked

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Raycast to move the boss in random directions
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 10f))
+        {
+            // If the raycast hits something, move the boss towards that direction
+            Vector3 targetPosition = hit.point;
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * 5f);
+        }
+        else
+        {
+            // If nothing is hit, move the boss in a random direction
+            Vector3 randomDirection = Random.insideUnitSphere * 5f;
+            transform.position += randomDirection * Time.deltaTime;
+        }
+    }
+}
